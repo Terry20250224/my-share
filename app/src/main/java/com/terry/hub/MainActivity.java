1fec7eebca406c42e7757e86adf0ffaf7356b5c2
Terry20250224 <wsc.terry@gmail.com>
2026-06-29 21:51:03 +0800

    Create MainActivity class for Android app

---
+++ app/src/main/java/com/terry/hub/MainActivity.java
@@ -0,0 +1,12 @@
+package com.terry.hub;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState) ;
+        // 這裡就是你未來放 WebView 的地方
+    }
+}
