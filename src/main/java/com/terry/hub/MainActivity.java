15b98d1337b5b7b82f14c566971503dccb8d826b
Terry <terry@terrywsc123.zo.computer>
2026-06-29 10:39:16 +0000

    新增 Android TerryHub 專案：build.gradle + Manifest + MainActivity

---
+++ src/main/java/com/terry/hub/MainActivity.java
@@ -0,0 +1,20 @@
+package com.terry.hub;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://terry20250224.github.io/my-share/links.html");
+        setContentView(webView);
+    }
+}
