2a3b3559bc5f8b14a1dae62491f50338e6291fe3
zocomputer <help@zocomputer.com>
2026-06-30 02:45:35 +0000

    Force commit layout changes

--- app/src/main/java/com/terry/hub/MainActivity.java
+++ app/src/main/java/com/terry/hub/MainActivity.java
@@ -1,29 +1,43 @@
 package com.terry.hub;
 
 import android.os.Bundle;
+import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView myWebView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        
-        // 1. 創建一個全螢幕的網頁瀏覽器元件
-        WebView webView = new WebView(this);
-        setContentView(webView);
-        
-        // 2. 開啟現代網頁必須要有的 JavaScript 功能
-        WebSettings webSettings = webView.getSettings();
+        setContentView(R.layout.activity_main);
+
+        myWebView = (findViewById(R.id.webview));
+        WebSettings webSettings = myWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         webSettings.setDomStorageEnabled(true);
-        
-        // 3. 確保點擊網頁內任何連結時，都在這個 App 裡面打開，而不是跳出瀏覽器
-        webView.setWebViewClient(new WebViewClient());
-        
-        // 4. 🔥 注入你的個人分享網站網址（讓它不再是一片空白！）
-        webView.loadUrl("https://terry20250224.github.io/my-share/links.html");
+        myWebView.setWebViewClient(new WebViewClient());
+        myWebView.loadUrl("https://terry20250224.github.io/my-share/links.html");
+
+        Button btnHome = findViewById(R.id.btn_home);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                recreate();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
